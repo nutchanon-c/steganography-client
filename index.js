c85016ea570d2cf1bcfa73e41585ca028b76fa3e
nutchanon-c <nutchanon.char@gmail.com>
2022-11-09 12:08:05 +0000

    aws upload, payloadd prep

--- index.js
+++ index.js
@@ -1,8 +1,73 @@
+require("dotenv").config();
 const { exec } = require("child_process");
+const { v4: uuidv4 } = require("uuid");
 const fs = require("fs");
 const path = require("path");
 const sg = require("any-steganography");
 const steganographyClass = require("any-steganography");
+const AWS = require("aws-sdk");
+
+const getKey = () => {
+  let keyFilePath = "./key.txt";
+  try {
+    if (fs.existsSync(keyFilePath)) {
+      let keyText = fs.readFileSync(keyFilePath).toString("utf-8");
+      console.log("Key file found and loaded");
+      return keyText;
+    } else {
+      console.log("Key file not found. Generating new key");
+      let keyGen = randomString(32);
+      fs.writeFileSync(keyFilePath, keyGen);
+      return keyGen;
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const getUUID = () => {
+  let uuidFilePath = "./uuid.txt";
+  try {
+    if (fs.existsSync(uuidFilePath)) {
+      let keyText = fs.readFileSync(uuidFilePath).toString("utf-8");
+      console.log("UUID file found and loaded");
+      return keyText;
+    } else {
+      console.log("UUID file not found. Generating new key");
+      let uuid = uuidv4();
+      fs.writeFileSync(uuidFilePath, uuid);
+      return uuid;
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const uuid = getUUID();
+const key = getKey();
+console.log(key);
+
+const s3 = new AWS.S3({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+});
+
+// console.log(uuidv4());
+
+const uploadFile = async (localFilePath, cloudFilePath) => {
+  const filename = localFilePath;
+  const fileContent = fs.readFileSync(filename);
+
+  const params = {
+    Bucket: process.env.AWS_BUCKET_NAME,
+    Key: `${uuid}/${cloudFilePath}`,
+    Body: fileContent,
+  };
+
+  const data = await s3.upload(params).promise(); // this line
+  console.log(`File uploaded successfully. ${data.Location}`);
+  return data.Location;
+};
 
 var args = process.argv.slice(2);
 console.log(args);
@@ -36,27 +101,6 @@ const executeTerminalCommand = (command) => {
 
 const checkABEKey = () => {};
 
-const getKey = () => {
-  let keyFilePath = "./key.txt";
-  try {
-    if (fs.existsSync(keyFilePath)) {
-      let keyText = fs.readFileSync(keyFilePath).toString("utf-8");
-      console.log("Key file found and loaded");
-      return keyText;
-    } else {
-      console.log("Key file not found. Generating new key");
-      let keyGen = randomString(32);
-      fs.writeFileSync(keyFilePath, keyGen);
-      return keyGen;
-    }
-  } catch (err) {
-    console.log(err);
-  }
-};
-
-const key = getKey();
-console.log(key);
-
 const encryptKeyWithABE = () => {
   let sessionKeyFilePath = "./key.txt";
   let abePubKeyPath = "../abe/pub_key";
@@ -80,9 +124,14 @@ const decryptKeyWithABE = () => {
 // encryptKeyWithABE();
 // decryptKeyWithABE();
 
-const uploadFileToCloud = (path) => {
-  // TODO: implement
+const uploadFileToCloud = async (path) => {
+  // console.log(path);
+  let filename = path.replace(/^.*[\\\/]/, "");
+  let url = await uploadFile(path, filename);
   console.log(path);
+  return url;
+
+  // TODO: implement
 };
 
 const loopFilesEncode = async (fileDir, key, text, outputDir) => {
@@ -95,11 +144,11 @@ const loopFilesEncode = async (fileDir, key, text, outputDir) => {
       // console.log(path.join(fileDir, file));
       stegaEncode(
         path.join(fileDir, file),
-        text.slice(i, i + parseInt(text.length / 5, 10)),
+        text.slice(i, i + text.length / files.length),
         path.join(outputDir, `${c}.jpg`),
         key
       );
-      i = i + parseInt(text.length / 5, 10);
+      i = i + text.length / files.length;
       c++;
     }
     return files;
@@ -151,49 +200,70 @@ function stegaDecode(inputFile, key) {
 
 var method = args[0];
 
-switch (method) {
-  case "enc":
-    if (args.length === 3) {
-      var ptPath = args[1];
-      var imageDirectory = args[2];
-      const file = ptPath;
-      const fileDir = imageDirectory + "/images/";
-      const encryptedDir = imageDirectory + "/output/";
-      console.log(file);
-      var text = fs.readFileSync(file).toString("utf-8");
-      // var textByLine = text.split("\n");
-      console.log(text.length);
-      loopFilesEncode(fileDir, key, text, encryptedDir);
-      let encryptedKeyPath = encryptKeyWithABE();
+async function main() {
+  switch (method) {
+    case "enc":
+      if (args.length === 3) {
+        var ptPath = args[1];
+        var imageDirectory = args[2];
+        const file = ptPath;
+        const fileDir = imageDirectory + "/images/";
+        const encryptedDir = imageDirectory + "/output/";
+        console.log(file);
+        var text = fs.readFileSync(file).toString("utf-8");
+        // var textByLine = text.split("\n");
+        console.log(text.length);
+        loopFilesEncode(fileDir, key, text, encryptedDir);
+        let encryptedKeyPath = encryptKeyWithABE();
 
-      try {
-        fs.promises.readdir(encryptedDir).then((files) => {
-          // console.log(files);
-          for (const file of files) {
-            // console.log(path.join(fileDir, file));
-            uploadFileToCloud(file);
-          }
-        });
-      } catch (e) {
-        console.error(e);
+        let payload = {
+          uuid: uuid,
+          files: [],
+          keyPath: "",
+        };
+
+        try {
+          fs.promises.readdir(encryptedDir).then((files) => {
+            // console.log(files);
+            for (const file of files) {
+              // console.log(path.join(fileDir, file));
+              uploadFileToCloud(`${encryptedDir}/${file}`).then((url) => {
+                let tmp = { ...payload };
+                tmp.files = [...tmp.files, url];
+                payload = { ...tmp };
+
+                uploadFileToCloud(encryptedKeyPath).then((url) => {
+                  let tmp = { ...payload };
+                  tmp.keyPath = url;
+                  payload = { ...tmp };
+                  console.log(payload);
+
+                  // TODO: SEND API TO MASTER
+                });
+              });
+            }
+          });
+        } catch (e) {
+          console.error(e);
+        }
+      } else {
+        console.log("please try again");
       }
-
-      uploadFileToCloud(encryptedKeyPath);
-    } else {
-      console.log("please try again");
-    }
-    break;
-  case "dec":
-    if (args.length === 2) {
-      var directory = args[1];
-      // var textByLine = text.split("\n");
-      // console.log(text.length);
-      loopFilesDecode(directory, key);
-    } else {
-      console.log("please try again");
-    }
-    break;
-  default:
-    console.log("unknown operation");
-    break;
+      break;
+    case "dec":
+      if (args.length === 2) {
+        var directory = args[1];
+        // var textByLine = text.split("\n");
+        // console.log(text.length);
+        loopFilesDecode(directory, key);
+      } else {
+        console.log("please try again");
+      }
+      break;
+    default:
+      console.log("unknown operation");
+      break;
+  }
 }
+
+main();
