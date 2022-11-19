9e5be624fd5532f94a9e661add8e14dc57e15aa9
nutchanon-c <nutchanon.char@gmail.com>
2022-11-19 05:29:17 +0000

    fix picture set algo

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
-require("dotenv").config();
-const { exec } = require("child_process");
-const { v4: uuidv4 } = require("uuid");
-const fs = require("fs");
-const path = require("path");
-const sg = require("any-steganography");
-const steganographyClass = require("any-steganography");
-const AWS = require("aws-sdk");
-var request = require("request");
+// require("dotenv").config();
+import dotenv from "dotenv";
+dotenv.config();
+// const { exec } = require("child_process");
+import { exec } from "child_process";
+// const { v4: uuidv4 } = require("uuid");
+import { v4 as uuidv4 } from "uuid";
+// const fs = require("fs");
+import fs from "fs";
+// const path = require("path");
+import path from "path";
+// const sg = require("any-steganography");
+import sg from "any-steganography";
+// const steganographyClass = require("any-steganography");
+// import { sg as steganographyClass } from "any-steganography";
+// const AWS = require("aws-sdk");
+import AWS from "aws-sdk";
+// var request = require("request");
+import request from "request";
+
+import fetch from "node-fetch";
 
 const getKey = () => {
   let keyFilePath = "./key.txt";
@@ -105,7 +117,7 @@ const checkABEKey = () => {};
 const encryptKeyWithABE = () => {
   let sessionKeyFilePath = "./key.txt";
   let abePubKeyPath = "../abe/pub_key";
-  let attr = "sysadmin";
+  let attr = "attr1";
 
   executeTerminalCommand(
     `cpabe-enc ${abePubKeyPath} ${sessionKeyFilePath} ${attr}`
@@ -126,29 +138,64 @@ const decryptKeyWithABE = () => {
   );
 };
 
+const decryptKeyWithABEWithPath = async (encSKPath, userAbeKeyPath) => {
+  console.log(`decrypting session key ${encSKPath}`);
+  let abePubKeyPath = "../abe/pub_key";
+  executeTerminalCommand(
+    `cpabe-dec ${abePubKeyPath} ${userAbeKeyPath} ${encSKPath}`
+  );
+};
+
+const downloadFile = async (url, fileName) => {
+  // const https = require("https"); // or 'https' for https:// URLs
+  // const fs = require("fs");
+  // const fetch = require("node-fetch");
+  if (!fs.existsSync("./downloads")) {
+    fs.mkdirSync("./downloads");
+  }
+  const file = fs.createWriteStream(`${fileName}`);
+  // https.get(url).then((response) => {
+  //   response.pipe(file);
+  //   // after download completed close filestream
+  //   file.on("finish", () => {
+  //     file.close();
+  //     console.log("Download Completed");
+  //     return `./downloads/${fileName}`;
+  //   });
+  //   return false;
+  // });
+  fetch(url).then((response) => {
+    response.body.pipe(file);
+    // after download completed close filestream
+    file.on("finish", () => {
+      file.close();
+      console.log("Download Completed");
+      return `${fileName}`;
+    });
+    return false;
+  });
+};
+
 // encryptKeyWithABE();
 // decryptKeyWithABE();
 
-const uploadFileToCloud = async (path) => {
+const uploadFileToCloud = async (path, setid) => {
   // console.log(path);
-
   let filename = path.replace(/^.*[\\\/]/, "");
   console.log(`uploading: ${filename}`);
-  let url = await uploadFile(path, filename);
+  let url = await uploadFile(path, `${setid}/${filename}`);
   // console.log(path);
   return url;
-
-  // TODO: implement
 };
 
 const loopFilesEncode = async (fileDir, key, text, outputDir) => {
   try {
     const files = await fs.promises.readdir(fileDir);
-    // console.log(files);
+    // console.log(`files: ${files}`);
     var i = 0;
     var c = 1;
     for (const file of files) {
-      // console.log(path.join(fileDir, file));
+      console.log(`encoding to file ${path.join(fileDir, file)}`);
       stegaEncode(
         path.join(fileDir, file),
         text.slice(i, i + text.length / files.length),
@@ -199,7 +246,7 @@ const loopFilesDecode = async (dir, key) => {
 
 function stegaDecode(inputFile, key) {
   const buffer = fs.readFileSync(inputFile);
-  const message = steganographyClass.default.decode(buffer, "jpg", key);
+  const message = sg.default.decode(buffer, "jpg", key);
   console.log(message);
   console.log("--------------------------------");
   return message;
@@ -232,55 +279,74 @@ async function main() {
           user_attributes: [],
         };
 
-        try {
-          fs.promises.readdir(encryptedDir).then(async (files) => {
-            // console.log(files);
-            let i = 1;
-            for (const file of files) {
-              console.log(`file: ${file}`);
-              // console.log(path.join(fileDir, file));
-              await uploadFileToCloud(`${encryptedDir}/${file}`).then((url) => {
-                allFilePath.push({ sequence: i, url: url });
-              });
-              i++;
-            }
+        await request.get(
+          `${process.env.API_URL}:3000/newID`,
+          function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+              let bodyJson = JSON.parse(body);
+              let set_id = bodyJson.id;
+              console.log(`top setid: ${bodyJson.id}`);
+              console.log(`returned new id body: ${body}`);
 
-            payload.files = allFilePath;
+              try {
+                fs.promises.readdir(encryptedDir).then(async (files) => {
+                  // console.log(files);
+                  let i = 1;
+                  for (const file of files) {
+                    console.log(`file: ${file}`);
+                    // console.log(path.join(fileDir, file));
+                    await uploadFileToCloud(
+                      `${encryptedDir}/${file}`,
+                      set_id
+                    ).then((url) => {
+                      allFilePath.push({ sequence: i, url: url });
+                    });
+                    i++;
+                  }
 
-            await uploadFileToCloud(encryptedKeyPath).then((url) => {
-              let tmp = { ...payload };
-              tmp.keyPath = url;
-              payload = { ...tmp };
-              console.log(payload);
-              payload.user_attributes = ["attr1", "attr2", "attr3"];
-              request.post(
-                `${process.env.API_URL}:3000/new`,
-                { json: payload },
-                function (error, response, body) {
-                  if (!error && response.statusCode == 200) {
-                    console.log(body);
-                    console.log(`setid: ${body.setID}`);
-                    fs.appendFile(
-                      `./picture_sets/${body.setID}.json`,
-                      `{"file":"${file}", "ps_id":"${body.setID}"}`,
-                      function (err) {
-                        if (err) throw err;
-                        console.log("Saved!");
+                  payload.files = allFilePath;
+
+                  await uploadFileToCloud(encryptedKeyPath, set_id).then((url) => {
+                    let tmp = { ...payload };
+                    tmp.keyPath = url;
+                    tmp.set_id = set_id;
+                    payload = { ...tmp };
+                    console.log(payload);
+                    payload.user_attributes = ["attr1", "attr2", "attr3"];
+                    request.post(
+                      `${process.env.API_URL}:3000/new`,
+                      { json: payload },
+                      function (error, response, body) {
+                        if (!error && response.statusCode == 200) {
+                          console.log(body);
+
+                          fs.appendFile(
+                            `./picture_sets/${set_id}.json`,
+                            `{"file":"${file}", "ps_id":"${set_id}"}`,
+                            function (err) {
+                              if (err) throw err;
+                              console.log("Saved!");
+                            }
+                          );
+                        } else {
+                          console.log("api error");
+                        }
                       }
                     );
-                  } else {
-                    console.log("api error");
-                  }
-                }
-              );
-            });
-          });
-        } catch (e) {
-          console.error(e);
-        }
+                  });
+                });
+              } catch (e) {
+                console.error(e);
+              }
+            } else {
+              console.log("api error");
+            }
+          }
+        );
       } else {
         console.log("please try again");
       }
+
       break;
     case "dec":
       if (args.length === 2) {
@@ -292,6 +358,49 @@ async function main() {
         console.log("please try again");
       }
       break;
+    case "req":
+      if (args.length === 2) {
+        var set_id = args[1];
+        request.post(`${process.env.API_URL}:3000/request`, {
+          json: { set_id: set_id, uuid: uuid },
+        });
+
+        request.post(
+          `${process.env.API_URL}:3000/request`,
+          { json: { set_id: set_id, uuid: uuid } },
+          function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+              let files = body.files;
+              let key_url = body.key_url;
+              console.log(`files ${files}`);
+              files.forEach((e) => {
+                console.log(e.url, e.sequence);
+              });
+              downloadFile(
+                key_url,
+                "./downloads/key/keytodecrypt.txt.cpabe"
+              ).then(() => {
+                files.forEach(async (element) => {
+                  await downloadFile(
+                    element.url,
+                    `./downloads/images/${element.sequence}`
+                  );
+                });
+
+                decryptKeyWithABEWithPath(
+                  "./downloads/key/keytodecrypt.txt.cpabe",
+                  "./test-sysadmin-key"
+                ).then(() => {
+                  let key = fs.readFileSync("./downloads/key/keytodecrypt.txt");
+                  loopFilesDecode("./downloads/images", key);
+                });
+              });
+            } else {
+              console.log("api error");
+            }
+          }
+        );
+      }
     default:
       console.log("unknown operation");
       break;
